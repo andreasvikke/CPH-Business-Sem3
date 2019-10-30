ba43db8723cac8695480c34ed74b2d21f57b4010
AndreasVikke <vikkedesign@gmail.com>
2019-10-30 08:29:49 +0100

    jhnkjlhn

--- Flow3/Week2/02-Tuesday/stateevent/src/App3.js
+++ Flow3/Week2/02-Tuesday/stateevent/src/App3.js
@@ -1,31 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
-    const [joke, setJoke] = useState("");
-    const [apiurl, setApiurl] = useState("https://icanhazdadjoke.com/");
+function FetchJoke(url) {
+    return fetch(url)
+    .then(response => response.json());
+}
 
-    useEffect(() => {
-        fetch(apiurl, {
-            headers: new Headers({
-                "Accept" : "application/json"
-            })
-        })
-        .then(response => response.json());
-    }, [apiurl]);
+function App() {
+    const [cnjoke, setCNJoke] = useState("");
+    const [dadjoke, setDadJoke] = useState("");
 
     useEffect(() => {
         const interval = setInterval(() => {
-            setApiurl("https://icanhazdadjoke.com/");
+            FetchJoke("https://icanhazdadjoke.com/slack").then(data => setDadJoke(data.attachments[0].text));
         }, 10000);
         return () => clearInterval(interval);
     });
 
     return (
         <div className="App">
-            <button onClick={() => setApiurl("https://api.chucknorris.io/jokes/random")}>Get Chuck Norris Joke</button>
-            <p><b>Chuck Norris Joke:</b> {cnJoke}</p>
-            <p><b>Dad Joke:</b> {dadJoke}</p>
+            <button onClick={() => FetchJoke("https://api.chucknorris.io/jokes/random").then(data => setCNJoke(data.value))}>Get Chuck Norris Joke</button>
+            <p><b>Chuck Norris Joke:</b> {cnjoke}</p>
+            <p><b>I Can Joke:</b> {dadjoke}</p>
         </div>
     )
 }
---
+++ Flow3/Week2/03-Wednesday/forms/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import FormDemoMultiple from './FormDemoMultiple';
+
+ReactDOM.render(<FormDemoMultiple />, document.getElementById('root'));
